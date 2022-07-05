6c801ca392d282212ef4f727eac95bc9fdc01e4e
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-07-05 20:18:00 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -5,4 +5,12 @@ let matrix = [
     [0, 1, -3]
 ]
 
-// Write code here. 
+let sum = 0;
+
+for (let i = 0; i < matrix.length; i++) {
+    if(matrix[i]<0 && matrix[i] % 2 ==0)
+    console.log(i);
+
+    sum = sum + i;
+    console.log(sum);
+} 
